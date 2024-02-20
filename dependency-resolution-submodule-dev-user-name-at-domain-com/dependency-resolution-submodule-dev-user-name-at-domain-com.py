5c99f7d8c091b52b99ac969f86fde95e8c59b9c5
skoulouzis <skoulouzis@users.noreply.github.com>
2024-02-20 12:25:29 +0100

    dependency-resolution-submodule-dev-user-name-at-domain-com creation

---
+++ dependency-resolution-submodule-dev-user-name-at-domain-com/dependency-resolution-submodule-dev-user-name-at-domain-com.py
@@ -0,0 +1,25 @@
+import os.path as path
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+
+subpath = path.join('.', 'subpath') 
+
+import json
+filename = "/tmp/subpath_" + id + ".json"
+file_subpath = open(filename, "w")
+file_subpath.write(json.dumps(subpath))
+file_subpath.close()
