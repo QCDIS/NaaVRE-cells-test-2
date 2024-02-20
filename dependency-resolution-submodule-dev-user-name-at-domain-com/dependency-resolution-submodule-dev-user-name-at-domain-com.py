e8a784e4a647f8b9638a3e67e0146da50fdd4a7d
skoulouzis <skoulouzis@users.noreply.github.com>
2024-02-20 14:03:46 +0100

    dependency-resolution-submodule-dev-user-name-at-domain-com update

--- dependency-resolution-submodule-dev-user-name-at-domain-com/dependency-resolution-submodule-dev-user-name-at-domain-com.py
+++ dependency-resolution-submodule-dev-user-name-at-domain-com/dependency-resolution-submodule-dev-user-name-at-domain-com.py
@@ -1,5 +1,4 @@
-import torch
-import torch.nn as nn
+import matplotlib.pyplot as plt
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -17,14 +16,21 @@ id = args.id
 
 
 
-linear_layer = nn.Linear(in_features=3, out_features=1)
+x_values = [1, 2, 3, 4, 5]
+y_values = [2, 4, 6, 8, 10]
 
-input_data = torch.tensor([[1.0, 2.0, 3.0]])
+plt.plot(x_values, y_values, label='Linear Function')
 
-output = str(linear_layer(input_data))
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Linear Plot')
+
+plt.legend()
+
+plt.show()
 
 import json
-filename = "/tmp/output_" + id + ".json"
-file_output = open(filename, "w")
-file_output.write(json.dumps(output))
-file_output.close()
+filename = "/tmp/x_values_" + id + ".json"
+file_x_values = open(filename, "w")
+file_x_values.write(json.dumps(x_values))
+file_x_values.close()
