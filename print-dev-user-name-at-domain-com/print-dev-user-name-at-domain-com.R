d970dc822e19afba3befc2f5a743e55678ee8fd0
skoulouzis <skoulouzis@users.noreply.github.com>
2024-02-23 14:11:08 +0100

    print-dev-user-name-at-domain-com creation

---
+++ print-dev-user-name-at-domain-com/print-dev-user-name-at-domain-com.R
@@ -0,0 +1,30 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--average"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+average = opt$average
+id <- gsub('"', '', opt$id)
+
+
+
+
+
+
+print(average)
+
+
+
